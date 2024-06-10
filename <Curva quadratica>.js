2e041c3cb76274fd1e69bf762f8e567f4528ef13
Victor Alcina 2L <161353502+VictorAlcina2LL@users.noreply.github.com>
2024-06-10 20:20:12 +0000

    Curvva quadratica

---
+++ <Curva quadratica>.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Curva quadratica</title>
+        <meta charset="UTF-8">
+
+        <script>
+            Function draw(){
+                var canvas = document.getElementById('meuCanvas');
+                if(canvas.getContext){
+                    var cntxt = canvas.getContext('2d');
+                    cntxt.beginPath();
+                    cntxt.filStyle = "rgb(0,255,0)";
+                    cntxt.moveTo(200,100);
+                    cntxt.quadraticCurveTo(150,200,200);
+                    cntxt.fill();
+                    cntxt.stroke();
+                    cmtxt.beginPath();
+                    cntxt.moveTo(200,100);
+                    cntxt.quadraticCurveTo(250,150,200,200);
+                    cntxt.fill();
+                    cntxt.stroke();
+                    cntxt.closePath();
+                }
+            }
+    </script>            
+    </head>
+    
